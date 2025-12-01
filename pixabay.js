28cfa85e2520c365a04511d93c17cf325282396a
BRENO DIAS MACHADO <25132475@CFP127ALUNO.LOCAL>
2025-12-01 09:23:21 -0300

    erro na hr de mostrar dados

--- pixabay.js
+++ pixabay.js
@@ -1,17 +1,37 @@
 'use strict';
 
 
-const seachImages = (text) => {
+
+const createCard = ({webformatURL}) => {
+    const card = document.createElement('div');
+    card.innerHTML = `
+    
+    <img src=${webformatURL}>
+    
+    `;
+
+    return  ;
+}
+
+const seachImages = async (text) => {
     const key = '53487813-9c88d26a545bade5140226556'
     const url = `https://pixabay.com/api/?key=${key}&q=${text}`
 
-    return url
+    const response = await fetch(url);
+    return response.json();
+    
+
+   
 
 }   
 
-const loadGallery = (text) => {
-    const imagensInfo = seachImages (text);
-    console.log(imagensInfo)
+const loadGallery = async (text) => {
+    const container = document.querySelector('.container-gallery')
+
+    const {hits} = await seachImages (text);
+    const cards = hits.map(createCard);
+    container.replaceChildren(cards)
+    console.log(cards)
 }
 
 const handleKeypress = ({key, target}) => {
