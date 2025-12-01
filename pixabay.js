ad2b1b808232bc88d3b606ea79893cf20aa1d716
BRENO DIAS MACHADO <25132475@CFP127ALUNO.LOCAL>
2025-12-01 11:24:39 -0300

    qs puxando as imagens

--- pixabay.js
+++ pixabay.js
@@ -8,13 +8,42 @@ const seachImages = async (text) => {
     return response.json();
 }   
 
+const createLink = (tag) => `
+<a hraf="#" onClick="loadGallery(${tag}">
+    ${tag}
+</a> 
+`
 
-const createCard = ({webformatURL}) => {
+const createCard = ({webformatURL, pageURL, tags, likes, comments }) => {
     const card = document.createElement('div');
+    card.classList.add('card-container')
     card.innerHTML = `
-    
+     <a href="${pageURL}" class="card-image">
     <img src=${webformatURL}>
-    
+    </a> 
+   <div class="card-info">
+        </div class="card-tags">
+                ${tags.split(',').map(createLink).join('')}
+        </div>
+
+            </div class="card-action">
+                <div class= "card-like"> 
+                    <i class="far fa-thumbs-up"> </i>
+                    <span>${likes} </span>
+                </div>
+
+                <div class= "card-comment"> 
+                    <i class="far fa-comment"> </i>
+                    <span>${comments} </span>
+                </div>
+
+                <div class="card-save">
+                    <i class="far fa-bookmark"></i>
+                </div>
+                
+             </div>
+             
+    </div>
     `;
 
     return  card;
@@ -25,7 +54,7 @@ const loadGallery = async (text) => {
 
     const {hits} = await seachImages (text);
     const cards = hits.map(createCard);
-    container.replaceChildren(cards);
+    container.replaceChildren(...cards);
     console.log(cards)
 }
 
